2161abda96f9a43ff5cf3437447c83ac671a45bf
Marco Martini <deckedspring@gmail.com>
2025-01-29 16:54:14 +0100

    Update start_gnome.sh
    Modify session execution command

--- podman/start_gnome.sh
+++ podman/start_gnome.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# This is based on hostname! Don't change it 
+# This is based on hostname! Don't change it
+USER=$(awk -F'-' '{print $2}' /etc/hostname)
+echo "Starting GNOME as $USER"
 setup_grd.sh
 su -c '(
     /usr/libexec/gnome-remote-desktop-daemon --headless &
-    gnome-session
-)' $(awk -F'-' '{print $2}' /etc/hostname)
-
+)' $USER
+/usr/libexec/gdm-headless-login-session --user $USER
