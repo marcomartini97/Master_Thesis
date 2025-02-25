d456c9721cb2e9bd1a2020c24d9090dac3728a35
Marco Martini <deckedspring@gmail.com>
2025-02-25 14:55:32 +0100

    Add containerfile, rework some scripts

--- podman/curl.sh
+++ podman/curl.sh
@@ -1,8 +1,8 @@
 curl --unix-socket /run/podman/podman.sock -X POST -H "Content-Type: application/json" -d '
 {
-    "name": "weston-marco",
-    "hostname": "weston-marco",
-    "image": "fedora_dev",
+    "name": "test-marco",
+    "hostname": "test-marco",
+    "image": "vdi-gnome-test",
     "cap_add": [
       "SYS_ADMIN",
       "NET_ADMIN",
@@ -27,37 +27,16 @@ curl --unix-socket /run/podman/podman.sock -X POST -H "Content-Type: application
       }
     ],
     "env": {
-      "XDG_RUNTIME_DIR": "/tmp",
-      "USERNAME": "marco"
+      "USERNAME": "marco",
+      "GSK_RENDERER": "ngl"
     },
-    "mounts":[
-        {
-                "Source": "/etc/weston",
-                "Destination": "/etc/weston",
-                "Type": "bind",
-                "ReadOnly": true
-        },
-      {
-        "Source": "/etc/passwd",
-        "Destination": "/etc/passwd",
-        "Type": "bind",
-        "ReadOnly": "true"
-      },
-      {
-        "Source": "/etc/group",
-        "Destination": "/etc/group",
-        "Type": "bind",
-        "ReadOnly": "true"
-      },
-      {
-        "Source": "/etc/shadow",
-        "Destination": "/etc/shadow",
-        "Type": "bind",
-        "ReadOnly": "true"
-      }
-
-    ],
-    "command": ["/usr/sbin/init"]
+    "mounts": [
+	    { "Source": "/etc/vdi", "Destination": "/etc/vdi", "Type": "bind", "ReadOnly": true },
+            { "Source": "/etc/passwd", "Destination": "/etc/passwd", "Type": "bind", "ReadOnly": true },
+            { "Source": "/etc/group", "Destination": "/etc/group", "Type": "bind", "ReadOnly": true },
+            { "Source": "/etc/shadow", "Destination": "/etc/shadow", "Type": "bind", "ReadOnly": true },
+            { "Source": "/home", "Destination": "/home", "Type": "bind" }],
+        "command": ["/usr/sbin/init"]
 }
 ' http://d/v5.3.0/libpod/containers/create
 
